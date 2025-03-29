ce68dd21752436cc23019d129e66b84d638736cc
l4mberjack <161288638+l4mberjack@users.noreply.github.com>
2025-03-29 12:15:00 +0300

    present 13 done(исправляем баг с рисованием базы), ошибка в ElementsDrawer.kt нужно исправить

--- app/src/main/java/com/example/tanchiki/enums/Material.kt
+++ app/src/main/java/com/example/tanchiki/enums/Material.kt
@@ -1,13 +1,63 @@
 package com.example.tanchiki.enums
 
+import com.example.tanchiki.R
+
+const val CELLS_SIMPLE_ELEMENT = 1
+const val CELLS_EAGLE_WIDTH = 4
+const val CELLS_EAGLE_HEIGHT = 3
+
 enum class Material(
     val tankConGoThrough: Boolean,
     val bulletCanGoThrough: Boolean,
-    val simpleBulletCanDestroy: Boolean
+    val simpleBulletCanDestroy: Boolean,
+    val canExistOnlyOne: Boolean,
+    val width: Int,
+    val height: Int,
+    val Image: Int
     ) {
-    EMPTY(true, true, true),
-    BRICK(false, false, true),
-    CONCRETE(false, false, false),
-    GRASS(true, true, false),
-    EAGLE(false, false, true),
+    EMPTY(
+        true,
+        true,
+        true,
+        false,
+        0,
+        0,
+        0
+    ),
+    BRICK(
+        false,
+        false,
+        true,
+        false,
+        CELLS_SIMPLE_ELEMENT,
+        CELLS_SIMPLE_ELEMENT,
+        R.drawable.brick
+    ),
+    CONCRETE(
+        false,
+        false,
+        false,
+        false,
+        1 ,
+        1,
+        R.drawable.concrete
+    ),
+    GRASS(
+        true,
+        true,
+        false,
+        false,
+        1,
+        1,
+        R.drawable.grass
+    ),
+    EAGLE(
+        false,
+        false,
+        true,
+        true,
+        4,
+        3,
+        R.drawable.eagle
+    ),
 }
--- app/src/main/java/com/example/tanchiki/models/Element.kt
+++ app/src/main/java/com/example/tanchiki/models/Element.kt
@@ -1,12 +1,12 @@
 package com.example.tanchiki.models
+import android.view.View
 import com.example.tanchiki.enums.Material
 
-data class Element(
-    val viewId:Int,
+data class Element constructor(
+    val viewId:Int = View.generateViewId(),
     val material: Material,
     val coordinate: Coordinate,
     val width: Int,
     val height: Int
 ){
-
 }
