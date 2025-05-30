ad5cbcae57465ef5c4ddde72baaf728256dac38b
l4mberjack <161288638+l4mberjack@users.noreply.github.com>
2025-05-30 13:01:17 +0300

    present 30 done

--- app/src/main/java/com/example/tanchiki/activities/ScoreActivity.kt
+++ app/src/main/java/com/example/tanchiki/activities/ScoreActivity.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tanchiki.R
+import com.example.tanchiki.databinding.ActivityMainBinding
+import com.example.tanchiki.sounds.ScoreSoundPlayer
 
 const val SCORE_REQUEST_CODE = 100
 
@@ -21,11 +23,14 @@ class ScoreActivity : AppCompatActivity() {
     }
 
     var score = 0
+    lateinit var binding: ActivityScoreBinding
 
     override fun OnCreate(){
         super.OnCreate(savedStateRegistry)
-        setContentView(R.layout.activity_score)
+        binding = ActivityScoreBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         score = intent.getIntExtra(EXTRA_SCORE, 0)
+        scoreSoundPlayer.playScoreSound()
     }
 
     override fun onBackPressed() {
@@ -34,4 +39,28 @@ class ScoreActivity : AppCompatActivity() {
         finish()
     }
 
+    private val scoreSoundPlayer by lazy{
+        ScoreSoundPlayer(this, soundReadyListener = {
+            startScoreCounting()
+        })
+    }
+
+    private fun startScoreCounting() {
+        Thread(Runnable {
+            var currentScore = 0
+            while(currentScore <= score){
+                runOnUiThread{
+                    binding.scoreTextView.text = currentScore.toString()
+                    currentScore += 100
+                }
+                Thread.sleep(150)
+            }
+            scoreSoundPlayer.pauseScoreSound()
+        }).start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scoreSoundPlayer.pauseScoreSound()
+    }
 }
